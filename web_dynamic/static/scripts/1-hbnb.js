e96e5f1e12351a66f328dbea5f3e10f50d5b6309
Dev-Loup <60360505+Dev-Loup@users.noreply.github.com>
2020-09-29 02:11:52 +0000

    [add]: js scripts and dynamic pages

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,13 @@
+const $ = window.$;
+$(document).ready(function () {
+  var objs = {};
+  $('input').change(function () {
+    if ($(this).is(':checked')) {
+      objs[$(this).attr('data-name')] = $(this).attr('data-id');
+    } else if ($(this).is(':not(:checked)')) {
+      delete objs[$(this).attr('data-name')];
+    }
+    const names = Object.keys(objs);
+    $('.amenities h4').text(names.sort().join(', '));
+  });
+});
